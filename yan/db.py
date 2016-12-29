94135c5b09b91adb9133113fa7c06124adfd61f1
Sergei Fomin <sergio-dna@yandex.ru>
2016-12-28 19:43:51 -0800

    Реализовал db.py

--- yan/db.py
+++ yan/db.py
@@ -1,13 +1,58 @@
 
+import logging
+
+from sqlalchemy import create_engine, Table, Column, Integer, \
+    String, MetaData, DateTime, Sequence
+
+from sqlalchemy.sql import select
+
+
 class OpenError(Exception): pass
 
 class SQLiteDAO:
 
     def __init__(self, filename):
-        pass
+        self._engine = create_engine('sqlite:///' + filename)
+        self._metadata = Metadata()
+        self._news = Table('news', metadata,
+            Column('id', Integer, Sequence('news_id_seq'), primary_key=True),
+            Column('title', String(100), nullable=False),
+            Column('text', String(500), nullable=False, index=True),
+            Column('topic', String(30)),
+            Column('cluster', String(100), nullable=False, index=True),
+            Column('datetime', DateTime),
+            Column('publisher', String(30)),
+        )
 
-    def getClusterByNewsTexts(self, texts):
-        pass
+        try:
+            self._metadata.create_all(self._engine)
+        except sqlalchemy.exc.OperationalError as exc:
+            raise OpenError(str(exc))
 
-    def getNews(self, **kw):
-        pass
+        self._conn = self._engine.connect()
+
+    def addNews(self, news_list):
+        news_list = self._preprocessNews(news_list)
+        self._conn.execute(self._news.insert(), news_list)
+
+    def _preprocessNews(self, news_list):
+        for news in news_list:
+            # Проверить, что строковые значения не слишком длинные
+            for column in self._news.columns:
+                if isinstance(column, String):
+                    name = column.name
+                    if len(news.get(name, "")) > column.type.length:
+                        logging.waning("обрезано значения для столбца '{}' (с {} до {})"
+                            .format(name, len(news[column]), column.type.length))
+                        news[column] = news[column][:column.type.length]
+        return news_list
+
+    def getClustersByNewsTexts(self, texts):
+        s = select([self._news.cluster]).where(self._news.c.texts.in_(texts))
+        result = self._conn.execute(s)
+        return [r[0] for r in result]
+
+    def getNewsByCluster(self, cluster):
+        s = select([self._news]).where(self._news.c.cluster == cluster)
+        result = self._conn.execute(s)
+        return [dict(row) for riw in result]
--- yan/yan.py
+++ yan/yan.py
@@ -21,12 +21,9 @@ def main():
     # Открыть базу данных
     try:
         dao = db.SQLiteDAO(args.file)
-    except IOError as exc:
-        logging.fatal("Не удалось открыть для записи файл {}: {}".format(args.file, exc.strerror))
+    except db.OpenError as exc:
+        logging.fatal("Не удалось открыть файл БД {}: {}".format(args.file, str(exc)))
         sys.exit(1)
-    except db.OpenError:
-        logging.fatal("Файл базы данных поврежден: {}".format(args.file))
-        sys.exit(2)
 
     # Выкачать новости
     news = www.YandexNews()
@@ -34,11 +31,11 @@ def main():
 
         # Проверим, не присутствует ли какая-то новость из имеющихся в базе данных.
         # Если присутствует, обновим метку кластера.
-        cluster = dao.getClusterByNewsTexts([news["text"] for news in news_list]) or cluster
+        cluster = dao.getClusterByNewsTexts([news["text"] for news in news_list])[0] or cluster
 
         # Составить список новостей, которые уже есть в БД
         # с этой меткой кластера
-        db_texts = set(news["text"] for news in dao.getNews(cluster=cluster))
+        db_texts = set(news["text"] for news in dao.getNewsByCluster(cluster))
 
         # Добавить все новости, которых в БД нет
         dao.addNews([news for news in news_list if news["text"] not in db_texts])
