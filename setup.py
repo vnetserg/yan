61375fa3735871882a9609f6776be2a7a9ccabc6
Sergei Fomin <sergio-dna@yandex.ru>
2017-01-10 18:07:34 -0800

    Обернул в Python-пакет

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import pip
+from setuptools import setup
+
+
+# Parse requirements:
+req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+requires = list(pip.req.parse_requirements(req_path, session=pip.download.PipSession()))
+
+setup(
+    name='yan',
+    version='0.1.0',
+    description='Yandex News feed parser',
+    author='Sergei Fomin',
+    author_email='sergio-dna@yandex.ru',
+    install_requires=[str(r.req) for r in requires if r.req],
+    dependency_links=[str(r.link) for r in requires if r.link],
+    py_modules=['yan']
+)
